f00ef28bc82815fb47bf2e0a1eb51e76d952ebd3
codelapp <info@codelapp.com>
2018-04-19 17:54:45 +0200

    Primer commit

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -1,25 +1,76 @@
-//
-//  ViewController.swift
-//  JSON
-//
-//  Created by Jorge Maldonado Borbón on 14/11/17.
-//  Copyright © 2017 Jorge Maldonado Borbón. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+struct User: Codable {
+    let id: Int
+    let name : String
+    let email: String
+    let address: Address
+}
 
+struct Address: Codable {
+    let city :String
+    let geo: Geo
+}
+
+struct Geo: Codable {
+    let lat: String
+}
+
+
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tabla: UITableView!
+    
+    var usuarios = [User]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tabla.delegate = self
+        tabla.dataSource = self
+        
+        datos()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usuarios.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tabla.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let usuario = usuarios[indexPath.row]
+        cell.textLabel?.text = usuario.name
+        cell.detailTextLabel?.text = usuario.address.geo.lat
+        return cell
+    }
+    
+    func datos(){
+        
+        let datos = "https://jsonplaceholder.typicode.com/users"
+        let url = URL(string: datos)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            do{
+                
+                self.usuarios = try JSONDecoder().decode([User].self, from: data!)
+                DispatchQueue.main.async {
+                    self.tabla.reloadData()
+                }
+                
+            }catch{
+                print("error al conectar")
+            }
+            
+        }.resume()
+        
+    }
 
 }
 
